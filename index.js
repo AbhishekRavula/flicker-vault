dfccf273d5d67d34182b9c9b907410402588db9b
AbhishekRavula <ravulaabhishek@gmail.com>
2024-10-29 14:01:38 +0530

    added RN paper and setup of custom Inter font

--- index.js
+++ index.js
@@ -2,8 +2,40 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import * as React from 'react';
+import {AppRegistry, useColorScheme} from 'react-native';
+import {
+  configureFonts,
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider,
+} from 'react-native-paper';
 import {name as appName} from './app.json';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+const fontConfig = {
+  fontFamily: 'Inter-VariableFont_opsz,wght',
+};
+
+const lightTheme = {
+  ...MD3LightTheme,
+  fonts: configureFonts({config: fontConfig}),
+};
+
+const darkTheme = {
+  ...MD3DarkTheme,
+  fonts: configureFonts({config: fontConfig}),
+};
+
+export default function Main() {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? darkTheme : lightTheme;
+
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
