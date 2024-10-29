a7d40598788b83ce641681096c185dc35660ada7
AbhishekRavula <ravulaabhishek@gmail.com>
2024-10-29 09:36:07 +0530

    Initial project setup

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Flicker Vault</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
