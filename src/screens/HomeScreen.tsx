3bf27d2d5b861ede8f80862bb8c118f56235362a
AbhishekRavula <ravulaabhishek@gmail.com>
2024-11-02 18:35:33 +0530

    movie details for home screen wall poster movie

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Pressable, SafeAreaView, StyleSheet, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {useQuery} from '@tanstack/react-query';
 import {
@@ -10,8 +10,12 @@ import {useAppTheme} from '../hooks/useAppTheme';
 import {useTranslation} from 'react-i18next';
 import {getImageUrl} from '../utils/commonUtils';
 import {MoviePosterSize} from '../constants/enums';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Movie, RootStackParamList} from '../constants/types';
 
-const HomeScreen = () => {
+type ProfileScreenProps = NativeStackScreenProps<RootStackParamList>;
+
+const HomeScreen = ({navigation}: ProfileScreenProps) => {
   const {theme} = useAppTheme();
   const {t} = useTranslation();
 
@@ -26,18 +30,32 @@ const HomeScreen = () => {
   });
 
   const moviesList = popularMovies.data?.results || [];
-  const wallPosterImage = trendingMovies.data?.results?.[0]?.poster_path || '';
+  const wallPosterMovie = trendingMovies.data?.results?.[0] as
+    | Movie
+    | undefined;
+
+  const onWallPosterPress = () => {
+    if (wallPosterMovie)
+      navigation.navigate('Details', {movie: wallPosterMovie});
+  };
 
   return (
     <SafeAreaView
       style={[styles.container, {backgroundColor: theme.colors.surface}]}>
-      <View style={styles.wallPosterContainer}>
-        <FastImage
-          source={{uri: getImageUrl(MoviePosterSize.w780, wallPosterImage)}}
-          style={styles.wallPoster}
-          resizeMode={FastImage.resizeMode.cover}
-        />
-      </View>
+      <Pressable style={styles.wallPosterContainer} onPress={onWallPosterPress}>
+        {wallPosterMovie && (
+          <FastImage
+            source={{
+              uri: getImageUrl(
+                MoviePosterSize.w780,
+                wallPosterMovie.poster_path,
+              ),
+            }}
+            style={styles.wallPoster}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+        )}
+      </Pressable>
       <MovieList title={t('Popular')} movies={moviesList} />
     </SafeAreaView>
   );
